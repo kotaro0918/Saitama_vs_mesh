20ece688b101719ef7d38c32fff21cef5020fa93
kotaro <big.i.land.k@gmail.com>
2023-03-03 00:03:35 +0900

    added 2-dimension-mesh

--- mesh.py
+++ mesh.py
@@ -2,14 +2,16 @@ import json
 import math
 import collections
 import sys
+import numpy as np
 lev = sys.argv
 mesh_list_1=[]
 mesh_list_2=[]
+mesh_list=[]
 def mesh_count_1(a):
     lg_result=str(math.floor(a[0]-100))
     la_aspect=str(math.floor(a[1])*(1.5)+(a[1]-math.floor(a[1]))*(0.9))
     la_result=la_aspect.split('.')[0]
-    mesh_list_1.append(la_result+lg_result)
+    mesh_list.append(la_result+lg_result)
 def mesh_count_2(a):
     lg_result_1=math.floor(a[0]-100)
     lg_result_2=int((a[0]-math.floor(a[0]))//0.125)
@@ -28,7 +30,7 @@ if lev[1] == "level1":
         coordinates_list=coordinates_dic['coordinates'][i][0]
         for j in range(len(coordinates_list)):
             mesh_count_1(coordinates_list[j])
-    c = collections.Counter(mesh_list_1)
+    c = collections.Counter(mesh_list)
     for key in sorted(c.keys()):
         print(key)
 if lev[1] == "level2":
@@ -38,10 +40,10 @@ if lev[1] == "level2":
         coordinates_list=coordinates_dic['coordinates'][i][0]
         for j in range(len(coordinates_list)):
             mesh_count_2(coordinates_list[j])
-    c = collections.Counter(mesh_list_1)
-    d = collections.Counter(mesh_list_2)
-    for key in sorted(c.keys()):
-        print(key)
-    for key in sorted(d.keys()):
-        print(key)
-    
+    (c,d) = (collections.Counter(mesh_list_1),collections.Counter(mesh_list_2))
+    (a,b) = (sorted(c.keys()),sorted(d.keys()))
+    print(a)
+    for i in b:
+        c=[j for j in a if 0<=(j-i)<=9]
+        mesh_list[len(mesh_list):len(mesh_list)]=(np.array(range(min(c),max(c)+1,1)))
+    print(mesh_list)
