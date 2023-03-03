ed4498f469564b6e4bb327c6d894e3bc03a9a6da
kotaro <big.i.land.k@gmail.com>
2023-03-03 10:21:23 +0900

    modified mesh.py

--- ex.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-a=[11,10,22,20,33,30]
-b=[10,20,30]
-mesh_list=[]
-for i in b:
-    c=[j for j in a if 0<=(j-i)<=9]
-    mesh_list[len(mesh_list):len(mesh_list)]=(np.array(range(min(c),max(c)+1,1)))
-print(mesh_list)
--- mesh.py
+++ mesh.py
@@ -17,8 +17,8 @@ def mesh_count_2(a):
     lg_result_2=int((a[0]-math.floor(a[0]))//0.125)
     la_result_1=int((a[1])*(60)//40)
     la_result_2=int(int((a[1])*(60)%40)//5)
-    mesh_list_1.append((la_result_1*10000+lg_result_1*100+la_result_2*10+lg_result_2))
-    mesh_list_2.append((la_result_1*10000+lg_result_1*100+la_result_2*10))
+    mesh_list_1.append(int(la_result_1*10000+lg_result_1*100+la_result_2*10+lg_result_2))
+    mesh_list_2.append(int(la_result_1*10000+lg_result_1*100+la_result_2*10))
 json_file=lev[2]
 json_open = open(json_file,'r')
 json_load = json.load(json_open)
@@ -42,7 +42,6 @@ if lev[1] == "level2":
             mesh_count_2(coordinates_list[j])
     (c,d) = (collections.Counter(mesh_list_1),collections.Counter(mesh_list_2))
     (a,b) = (sorted(c.keys()),sorted(d.keys()))
-    print(a)
     for i in b:
         c=[j for j in a if 0<=(j-i)<=9]
         mesh_list[len(mesh_list):len(mesh_list)]=(np.array(range(min(c),max(c)+1,1)))
